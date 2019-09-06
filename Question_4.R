2e4c87dbed17152a638febb369ff7c8118b78915
HaipengLin-44451253 <54321725+HaipengLin-44451253@users.noreply.github.com>
2019-09-06 12:02:58 +1000

    Update Question_4.R

--- Question_4.R
+++ Question_4.R
@@ -1,5 +1,13 @@
 ##Q4
 ## For A
+### For A
+##The individual's birthday is 16/05/1995.
+
+##The age next birthday at death of the individual is 16/05/1995, whose age is 30 years old.
+
+##The age last birthday an 1 January preceding death of the individual is 16/05/1993, whose age is 28 years old.
+
+##The age at the birthday in the policy year of the death of the individual is 16/05/1994, whose age is 29 years old.
 ###a
 1995-1965
 ###b
@@ -9,6 +17,13 @@
 
 
 ## For B
+##The individual's birthday is 01/03/1922.
+
+##The age next birthday at death of the individual is 01/03/1994, whose age is 72 years old.
+
+##The age last birthday an 1 January preceding death of the individual is 01/03/1992, whose age is 70 years old.
+
+##The age at the birthday in the policy year of the death of the individual is 01/03/1993, whose age is 71 years old.
 ###a
 1994-1922
 ###b
@@ -18,6 +33,13 @@
 
 
 ## For C
+##The individual's birthday is 21/09/1942.
+
+##The age next birthday at death of the individual is 21/09/1996, whose age is 54 years old.
+
+##The age last birthday an 1 January preceding death of the individual is 21/09/1995, whose age is 53 years old.
+
+##The age at the birthday in the policy year of the death of the individual is 21/09/1995, whose age is 53 years old.
 ###a
 1996-1942
 ###b
