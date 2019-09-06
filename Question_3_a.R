b1c8ab86e6309044d358ce093052c59357794e96
HaipengLin-44451253 <54321725+HaipengLin-44451253@users.noreply.github.com>
2019-09-06 11:58:38 +1000

    Update Question_3_a.R

--- Question_3_a.R
+++ Question_3_a.R
@@ -1,5 +1,9 @@
 ##Q3
 ###a
+##As the case given, the annual commission is 1000 dollars and the interest rate is 3.5%. 
+##The present value of the total commmission is given by$$PV_{comm}=x_{comm}\frac{1-v^n}{i},\qquad v=\frac{1}{1+i}$$
+##Therefore, the PV of the total commmission is $18392.05.
+
 p<-32622.8;n=30;i<-0.035;loan<-600000
 
 comm<-1000
